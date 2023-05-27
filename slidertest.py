0ca8ee8d4a8a1f839f0a320efa447c40f919a485
daimiura <daisuke.miura@riken.jp>
2023-05-27 17:47:11 +0900

    updated

--- slidertest.py
+++ slidertest.py
@@ -97,7 +97,7 @@ lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc='lower right')
 
 # Rotate the x-axis tick labels for better readability
-plt.xticks(rotation=90)
+fig.autofmt_xdate(rotation=90)
 
 # Display the graph
 st.pyplot(fig)
